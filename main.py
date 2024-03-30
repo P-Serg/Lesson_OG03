ab5cc711ebe2d56386639dab76a5dcf47cf51942
P-Serg <163842794+P-Serg@users.noreply.github.com>
2024-03-30 18:20:58 +0500

    errors are fixed

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.tupe == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
